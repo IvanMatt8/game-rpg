3e4e4b38b780ee25124a32dbf87f2611b2126def
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-28 13:28:57 -0300

    :sparkles: adicionado estilo para aplicação.

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,3 +1,7 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background-color: #24282f;
+  min-height: 100vh;
+  color: #fff;
+`;
