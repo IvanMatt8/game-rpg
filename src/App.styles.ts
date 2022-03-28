b6a497286921145d76a1de4e32144135ecefcb17
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-28 13:42:11 -0300

    :sparkles: adicionado estilo para o mapa do game

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,7 +1,18 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: #24282f;
   min-height: 100vh;
   color: #fff;
 `;
+
+export const Map = styled.div`
+  width: 780px;
+  height: 780px;
+  background-image: url("/assets/map.png");
+  background-position: cover;
+  background-size: 100%;
+`; 
