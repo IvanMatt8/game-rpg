699137b75926ff629ca7e53a1218cf33bbe4bed3
IvanMatt8 <ivanmatt1995@gmail.com>
2022-03-28 15:22:14 -0300

    :construction: adicionando função de mover o personagem

--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -1,39 +1,57 @@
 import { useState } from "react";
+import { CharacterSides } from "../types/CharacterSides";
+import { mapSpots } from "../data/map.Spots";
 
 export const useCharacter = () => {
   const [pos, setPos] = useState({ x: 3, y: 5 });
+  const [side, setSide] = useState<CharacterSides>("down");
 
   const moveLeft = () => {
-    setPos((pos) => ({
-      x: pos.x - 1,
-      y: pos.y,
-    }));
+    if (canMove(pos.x - 1, pos.y))
+      setPos((pos) => ({
+        x: pos.x - 1,
+        y: pos.y,
+      }));
+    setSide("left");
   };
 
   const moveRight = () => {
-    setPos((pos) => ({
-      x: pos.x + 1,
-      y: pos.y,
-    }));
+    if (canMove(pos.x + 1, pos.y))
+      setPos((pos) => ({
+        x: pos.x + 1,
+        y: pos.y,
+      }));
+    setSide("right");
   };
 
   const moveDown = () => {
-    setPos((pos) => ({
-      x: pos.x,
-      y: pos.y + 1,
-    }));
+    if (canMove(pos.x, pos.y + 1))
+      setPos((pos) => ({
+        x: pos.x,
+        y: pos.y + 1,
+      }));
+    setSide("down");
   };
 
   const moveUp = () => {
-    setPos((pos) => ({
-      x: pos.x,
-      y: pos.y - 1,
-    }));
+    if (canMove(pos.x, pos.y - 1))
+      setPos((pos) => ({
+        x: pos.x,
+        y: pos.y - 1,
+      }));
+    setSide("up");
+  };
+
+  const canMove = (x: number, y: number) => {
+    if (x < 0 || y < 0) return false;
+
+    return true;
   };
 
   return {
     x: pos.x,
     y: pos.y,
+    side,
     moveLeft,
     moveRight,
     moveDown,
